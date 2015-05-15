cdf9e2ac77b7a2c1dd09c5ea933f15417b68f669
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2015-05-15 19:23:43 +0000

    About to add Alice/Christel changes
    git-svn-id: https://microb98.med.upenn.edu/svn/scott/thesis@9942 acd2042c-187b-11de-8279-d8d34a11d372

--- thesis/generateAbstract.bash
+++ thesis/generateAbstract.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -n  '/%%ABSTRACT_START%%/,/%%ABSTRACT_END%%/p' $1|sed '/%%ABSTRACT_[^%]\+%%/d' |sed '/^[ \t]*%.*$/d'|sed 's@\\hivEight{}@HIV89.6@g'|sed 's/\$//g'
+sed -n  '/%%ABSTRACT_START%%/,/%%ABSTRACT_END%%/p' $1|sed '/%%ABSTRACT_[^%]\+%%/d' |sed '/^[ \t]*%.*$/d'|sed 's@\\hivEight{}@HIV89.6@g'|sed 's/\$//g'|sed 's/^[\t ]\+//g'
